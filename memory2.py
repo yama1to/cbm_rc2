0aec425f15f69258713f5f0eb0e5b1dbe58be4a9
sakino yamato <sakino@sakino.local>
2022-03-20 20:20:41 +0900

    update

--- memory2.py
+++ memory2.py
@@ -4,15 +4,67 @@ import numpy as np
 from generate_datasets import generate_data_sequence as data
 from generate_matrix import *
 from utils import *
-
+from explorer import *
 from _network import ReservoirComputingbasedonChaoticBoltzmannMachine as CBM
 
-if __name__ == '__main__':
+class Config():
+    def __init__(self):
+        # columns, csv, id: データの管理のために必須の変数
+        self.columns = None # 結果をCSVに保存する際のコラム
+        self.csv = None # 結果を保存するファイル
+        self.id  = None
+        self.plot = 1 # 図の出力のオンオフ
+        self.show = False # 図の表示（plt.show()）のオンオフ、explorerは実行時にこれをオフにする。
+        self.savefig = False
+        self.fig1 = "fig1.png" ### 画像ファイル名
+
+        # config
+        self.dataset=6
+        self.seed:int=1 # 乱数生成のためのシード
+        self.NN=2**8 # １サイクルあたりの時間ステップ
+        self.MM=2200 # サイクル数
+        self.MM0 = 200 #
+
+        self.Nu = 1         #size of input
+        self.Nh:int = 100   #815 #size of dynamical reservior
+        self.Ny = 20        #size of output
+
+        self.Temp=1
+        self.dt=1.0/self.NN #0.01
+
+        #sigma_np = -5
+        self.alpha_i = 0.24
+        self.alpha_r = 0.9
+        self.alpha_b = 0.
+        self.alpha_s = 0.5
+
+        self.alpha0 = 0#0.1
+        self.alpha1 = 1#-5.8
+
+        self.beta_i = 0.9
+        self.beta_r = 0.02
+        self.beta_b = 0.
+
+        self.lambda0 = 0.
+
+        self.delay = 20
+
+        # ResultsX
+        self.RMSE1=None
+        self.RMSE2=None
+        self.MC = None
+        self.MC1 = None 
+        self.MC2 = None
+        self.MC3 = None
+        self.MC4 = None
+        self.cnt_overflow=None
+
+def execute(c):
     MM=2200
     delay=20
 
     if True:
-        T = MM
+        T = c.MM
         #U,D = generate_white_noise(c.delay,T=T+200,)
         U,D = data.generate_white_noise(delay,T=T+200,dist="uniform")
         Up=U[200:]
@@ -24,7 +76,14 @@ if __name__ == '__main__':
             Dp /= max*2
             Up /= max*2
 
-    model = CBM(MM=MM)
+    model = CBM(columns = c.columns,csv = c.csv,id  = c.id,
+                plot = c.plot,show = c.show,savefig = c.savefig,fig1 = c.fig1,
+                dataset=c.dataset,seed=c.seed,
+                NN=c.NN,MM=c.MM,MM0 = c.MM0,Nu = c.Nu,Nh= c.Nh,Ny = c.Ny,Temp=c.Temp,
+                alpha_i = c.alpha_i,alpha_r = c.alpha_r,alpha_b = c.alpha_b,alpha_s = c.alpha_s,
+                beta_i = c.betai,beta_r = c.beta_r,beta_b = c.beta_b,
+                lambda0 = c.lambda0,delay = c.delay)
+
     model.generate_network()
     model.fit(train_data=Up,target_data=Dp)
 
@@ -36,3 +95,13 @@ if __name__ == '__main__':
     plot_MC(Yp,Dp,delay=delay,show = 1,save=1,dir_name = "trashfigure",fig_name="mc1")
 
 
+
+if __name__ == '__main__':
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-config", type=str)
+    a = ap.parse_args()
+    
+    c=Config()
+    if a.config: c=common.load_config(a)
+    execute(c)
+    if a.config: common.save_config(c)
